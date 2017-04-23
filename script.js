fb4c230c24ca12deb8b59213365561c7752679a9
Eitan Sheena <esheena1@gmail.com>
2017-04-22 23:34:20 -0400

    Added functional color changer and eraser

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var inputx = prompt('How big do you want the canvas to be? (Please enter a value from 16 - 100)');
-while (inputx < 16 ||inputx > 100) {
+while (inputx < 16 || inputx > 100) {
   inputx = prompt('That was not a valid size, please enter a value from 16 - 100');
 }
 var inputy = inputx;
@@ -7,15 +7,13 @@ var inputy = inputx;
 dim = inputx * 10;
 
 function changewidth() {
-document.getElementById("container").style.height = dim +'px';
-    document.getElementById("container").style.width = dim + 'px';
+  document.getElementById("container").style.height = dim + 'px';
+  document.getElementById("container").style.width = dim + 'px';
 }
-
-
 
 $(document).ready(function() {
 
- changewidth();
+  changewidth();
 
   for (var x = 0; x < inputx; x++) {
     for (var y = 0; y < inputy; y++) {
@@ -23,17 +21,52 @@ $(document).ready(function() {
       unit.appendTo('#container');
     }
   }
+
+  var color = ['blue', 'red', 'green', 'erase'];
+  color = color[0];
+
+
+  $("#bluebtn").click(function() {
+    color = 'blue';
+
+  });
+  $("#redbtn").click(function() {
+    color = 'red';
+
+  });
+  $("#greenbtn").click(function() {
+    color = 'green';
+
+  });
+  $("#erase").click(function() {
+    color = 'erase';
+
+  });
+
+
+
   mouseIsPressed = false;
   $('.unit').mousedown(function() {
-  	mouseIsPressed = true;
+    mouseIsPressed = true;
   });
   $('.unit').mouseup(function() {
-  mouseIsPressed = false;
-  })
-  $('.unit').mouseover(function() {
-  if (mouseIsPressed) {
-  	$(this).removeClass().addClass('newunitblue')
-  }
+    mouseIsPressed = false;
+  });
 
-});
+  $('.unit').mouseover(function() {
+    if (mouseIsPressed) {
+      if (color === 'blue') {
+        $(this).removeClass().addClass('newunitblue')
+      } else if (color === 'red') {
+        $(this).removeClass().addClass('newunitred')
+      } else if (color === 'green') {
+        $(this).removeClass().addClass('newunitgreen')
+      } else if (color === 'erase') {
+        $(this).removeClass().addClass('newuniterase')
+      }
+    }
+
+  });
+
+
 });
